c28c8d400d70733cfd914a55f4868bc1b7772ddd
daviddoherty10 <daviddoherty2016@gmail.com>
2023-08-06 17:16:12 +0100

    v

--- new_user.py
+++ new_user.py
@@ -6,31 +6,29 @@ class User:
         self.username = username
         self.password = password
     
-import csv
-import re
 
-def make_username():
-    while True:
-        username = input("Username: ").rstrip()
-        if not username:
-            print("Please input a valid username")
-        elif not re.search(r"\d\w\S", username) or not re.search(r"[a-zA-Z][0-9]\S", username):
-            print("Invalid username. Usernames must contain both letters and digits.")
-        else:
-            with open("user info.csv", mode="r") as file:
-                csv_reader = csv.DictReader(file)
-                for row in csv_reader:
-                    if row["username"] == username:
-                        print("Username already exists.")
-                        continue
+    def make_username():
+        while True:
+            username = input("Username: ").rstrip()
+            if not username:
+                print("Please input a valid username")
+            elif re.search(r"[a-zA-Z][0-9]\S", username):
+                print("Invalid username. Usernames must contain both letters and digits.")
+            else:
+                with open("user info.csv", mode="r") as file:
+                    csv_reader = csv.DictReader(file)
+                    for row in csv_reader:
+                        if row["username"] == username:
+                            print("Username already exists.")
+                            break
 
-                    happy = input("Make this your username? (yes/no) ").lower().rstrip()
-                    if happy == "yes":
-                        return username
+                        happy = input("Make this your username? (yes/no) ").lower().rstrip()
+                        if happy == "yes":
+                            return username
+                        else:
+                            continue  # Exit the loop and reprompt for a new username
                     else:
-                        continue  # Exit the loop and reprompt for a new username
-                else:
-                    return username  # If the loop completes without finding a match, the username is available
+                        return username  # If the loop completes without finding a match, the username is available
 
 
     def make_password():
